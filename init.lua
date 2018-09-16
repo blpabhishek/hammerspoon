b2b076e65d2463a3fb24e53c7631e72a1697614a
Abhishek Gupta <abhishkg@thoughtworks.com>
2018-09-16 23:29:32 +0530

    Add Reload config shortcut

--- init.lua
+++ init.lua
@@ -1,21 +1,27 @@
 require "screen"
 require "wifi"
 
+-- Load Caffeine
+
 hs.loadSpoon("Caffeine")
 spoon.Caffeine:start()
-
 hs.hotkey.bind({"cmd", "alt", "ctrl"}, "c", function()
   spoon.Caffeine:start()
 end)
-
 hs.hotkey.bind({"cmd", "alt", "ctrl"}, "d", function()
   spoon.Caffeine:stop()
 end)
 
+-- Clipboard shortcut
+
 hs.loadSpoon("ClipboardTool")
-
 spoon.ClipboardTool:start()
-
 spoon.ClipboardTool:bindHotkeys({
   toggle_clipboard = { { "ctrl","shift" }, "v" }
 })
+
+-- Reload Hamerspoon 
+hs.hotkey.bind({"cmd", "alt", "ctrl"}, "R", function()
+  hs.reload()
+end)
+hs.notify.new({title="Hammerspoon", informativeText="Config Reloaded"}):send()
